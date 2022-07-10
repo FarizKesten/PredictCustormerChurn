29ba623b8daf0147e14a54151253515cc9700fe0
kfs1fe <fariz.kesten@de.bosch.com>
2022-07-10 22:34:44 +0200

    Add tests

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 # Predict Customer Churn
-
-- Project **Predict Customer Churn** of ML DevOps Engineer Nanodegree Udacity
+Project **Predict Customer Churn** of ML DevOps Engineer Nanodegree Udacity
 
 ## Project Description
-Your project description here.
+Try to identify credit card customers that are most likely to churn.
 
 ## Files and data description
-Overview of the files and data present in the root directory. 
+`churn_library.py`: main script that runs the training & reporting
+
+`models/`: models saved during the trainings
+
+`images/eda`: Analysis plots on the data
+
+`images/results`: Result data from the training
 
 ## Running Files
-How do you run your files? What should happen when you run your files?
+Some libraries might need to be installed in your environment in order to run the main script (`churn_library.py`). Refer to requirements_pyxxx.txt to learn about which libraries are needed.( Or call `pip install -r requirements_pyxxx.txt` to install the libraries)
 
+Use autopep8 via command lines in case you made some changes and want to make sure they
+follows pep8-guidelines:
 
+`autopep8 --in-place --aggressive --aggressive churn_library.py`
+
+To test the code you could run the test script, either:
+`ipython churn_script_logging_and_tests.py` 
+
+or using pytest:
+`pytest churn_script_logging_and_tests.py`
 
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -1,6 +1,7 @@
 import os
 import logging
-import churn_library_solution as cls
+from math import ceil
+from churn_library import *
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
@@ -8,7 +9,7 @@ logging.basicConfig(
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
+def test_import():
 	'''
 	test data import - this example is completed for you to assist with the other test functions
 	'''
@@ -16,7 +17,7 @@ def test_import(import_data):
 		df = import_data("./data/bank_data.csv")
 		logging.info("Testing import_data: SUCCESS")
 	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
+		logging.error("Testing import_data: The file wasn't found")
 		raise err
 
 	try:
@@ -27,37 +28,91 @@ def test_import(import_data):
 		raise err
 
 
-def test_eda(perform_eda):
+def test_eda():
 	'''
 	test perform eda function
 	'''
+	df = import_data("./data/bank_data.csv")
+	try:
+		perform_eda(df)
+		logging.info("Testing perform_eda: SUCCESS")
+	except KeyError as err:
+		logging.error("Testing perform_eda:ERROR %s", err)
+
+	files = ["churn_histogram.png", "customer_age_histogram.png",
+             "marital_status.png", "total_trans_density.png"]
+	for file in files:
+		try:
+			assert os.path.exists("images/eda/" + file) is True
+			logging.info("Testing perform_eda: %s was found", file)
+		except AssertionError as err:
+			logging.error("Testing perform_eda: %s was not found", file)
+			raise err
 
 
-def test_encoder_helper(encoder_helper):
+
+def test_encoder_helper():
 	'''
 	test encoder helper
 	'''
+	df = import_data("./data/bank_data.csv")
+	cat_columns = ['Gender', 'Education_Level', 'Marital_Status',
+                   'Income_Category', 'Card_Category']
 
+	try:
+		encoded_data = encoder_helper(df, cat_columns, response=None)
+		assert encoded_data is not None #new data is not None
+		assert encoded_data.equals(df) is False # new data is different then old data
+		assert encoded_data.columns.equals(df.columns) is True # columns stay the same
+		logging.info("Testing encoder_helper: SUCCESS")
+	except AssertionError as err:
+		logging.error("Testing encoder_helper:FAILED %s", err)
+		raise err
 
-def test_perform_feature_engineering(perform_feature_engineering):
+def test_perform_feature_engineering():
 	'''
 	test perform_feature_engineering
 	'''
+	df = import_data("./data/bank_data.csv")
+	try:
+		x_trn, x_tst, y_trn, y_tst = perform_feature_engineering(df, response='Churn')
+		# all data should not be empty
+		assert x_trn is not None
+		assert x_tst is not None
+		assert y_trn is not None
+		assert y_tst is not None
+		# test dataset ~30% whole data
+		assert (x_tst.shape[0] == ceil(0.3*df.shape[0])) is True
+		logging.info("Testing perform_feature_engineering: SUCCESS")
+	except AssertionError as err:
+		logging.error('Testing perform_feature_engineering: FAILED %s', err)
+		raise err
 
 
-def test_train_models(train_models):
+
+def test_train_models():
 	'''
 	test train_models
 	'''
+	bank_df = import_data(r"./data/bank_data.csv")
+	x_trn, x_tst, y_trn, y_tst = perform_feature_engineering(bank_df,
+                                 response='Churn')
+
+	try:
+		train_models(x_trn, x_tst, y_trn, y_tst)
+		assert(os.path.exists('./images/results/roc.png')) is True
+		assert(os.path.exists('./models/rfc_model.pkl')) is True
+		assert(os.path.exists('./models/logistic_model.pkl')) is True
+		logging.info("Testing train_models: SUCCESS")
+	except AssertionError as err:
+		logging.error('file not found %s', err)
+		raise err
+
 
 
 if __name__ == "__main__":
-	pass
-
-
-
-
-
-
-
-
+	test_import()
+	test_eda()
+	test_encoder_helper()
+	test_perform_feature_engineering()
+	test_train_models()
